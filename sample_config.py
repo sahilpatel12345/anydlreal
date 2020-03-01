83b3ab983f2912710cb66032cf51f71d00dba4dc
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-03-01 19:20:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,7 @@ class Config(object):
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    DOWNLOAD_LOCATION = "./mybabadl"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 1572864000
