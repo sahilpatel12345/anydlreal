67e7b627cf49b09e6a03934754fecf519d1bed90
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-03-07 15:15:28 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -45,7 +45,7 @@ async def ddl_call_back(bot, update):
     thumb_image_path = Config.DOWNLOAD_LOCATION + \
         "/" + str(update.from_user.id) + ".jpg"
     youtube_dl_url = update.message.reply_to_message.text
-    custom_file_name = os.path.basename(youtube_dl_url)
+    custom_file_name = "@GTMovies " + os.path.basename(youtube_dl_url)
     if "|" in youtube_dl_url:
         url_parts = youtube_dl_url.split("|")
         if len(url_parts) == 2:
